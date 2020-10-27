da469bbc97f048c876358d039f464ab37c90ddfd
xXxSovereign <michael.w.adolph@gmail.com>
2020-10-26 21:37:08 -0400

    Create test.py

--- SandboxTestFile.py
+++ SandboxTestFile.py
@@ -2,6 +2,8 @@
 
 # I.E. test any random thing here, an open area so you dont clutter your own files
 
+# If code is left here, it may be deleted by you or I, if its important don't leave it here
+
 test = [
     [1, 2, 3, 4, 5],
     [1, 2, 3, 4, 5],
