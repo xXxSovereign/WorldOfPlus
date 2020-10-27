17b710e155e2a06549bda54fdc3085e20e2dbe93
xXxSovereign <michael.w.adolph@gmail.com>
2020-10-27 16:03:27 -0400

    test commit

--- commands/mapStuff.py
+++ commands/mapStuff.py
@@ -24,8 +24,7 @@ def gen_Map(size):
         mapSize = 15  # Middle: [7][7]
         center = [7, 7]
 
-    mapv1 = [["+" for _ in range(mapSize)] for _ in range(mapSize)]  # Creating the map as a list, _ means that no var
-    mapv1[center[0]][center[1]] = "H"                                # is needed
+    mapv1 = [["+" for _ in range(mapSize)] for _ in range(mapSize)]  # Creating the map as a list, _ means that no var                               # is needed
 
     mapv2 = mapv1.copy()
     dungeon_count = 0
