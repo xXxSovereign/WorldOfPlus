4167c25b834e5c05e74c67506dc219dc88b352bc
xXxSovereign <michael.w.adolph@gmail.com>
2020-10-26 21:25:25 -0400

    Create test.py

---
+++ SandboxTestFile.py
@@ -0,0 +1,15 @@
+# THIS IS A FILE WHERE YOU CAN TEST ANYTHING YOU WANT. THIS WILL BE DELETED UPON RELEASE
+
+# I.E. test any random thing here, an open area so you dont clutter your own files
+
+test = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, "center", 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+]
+
+print(test[2][2])  # should fetch the "center" at the direct center
+
+# and it works haha im a magician
--- commands/mapStuff.py
+++ commands/mapStuff.py
@@ -1,11 +1,23 @@
-def display_Map(worldMap):
+import os
 
-    for i in worldMap:
-        str = ""
+
+def display_Map(world_map):  # Function to display the world map, parameter is the current explored map
+    os.system("cls")  # Clears previous text in the Terminal
+    for i in world_map:
+        output = ""  # For loop to access individual items in the map list
         for j in i:
-            str += j + " "
-        print(str)
+            output += j + " "
+        print(output)  # Outputs the map in a square
 
 
-def gen_Map():
-    return [["+" for i in range(10)] for j in range(10)]
+def gen_Map(size):
+    # Determine map size off of user input "size"
+    if size == 1:
+        mapSize = 7
+        center = [3, 3]  # Middle of square will be: [3][3]
+    elif size == 2:
+        mapSize = 11  # Middle: [5][5]
+    elif size == 3:
+        mapSize = 15  # Middle: [7][7]
+
+    [["+" for i in range(mapSize)] for j in range(mapSize)]  # Creating the map as a list
