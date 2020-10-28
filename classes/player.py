5401361b4ec9af00a82791d98f5e9992b35829a1
jimenezj-shs <jimenezjessej@scps.net>
2020-10-28 17:22:38 -0400

    lists in classes are weird. ill figure out more of this later.

--- classes/player.py
+++ classes/player.py
@@ -1,3 +1,13 @@
 class Player:
     def __init__(self):
-        self.lol = 54
+        # Theses values are all place holders
+
+        self.inventory = []
+
+    def add_item(self, inventory):
+        self.inventory.append(inventory)
+
+
+player1 = Player()
+player1.add_item("Wooden Sword")
+print(player1.inventory)
