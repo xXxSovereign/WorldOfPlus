e1e3270ae5734c098c5e5020b7955e3194f00e8f
xXxSovereign <michael.w.adolph@gmail.com>
2020-10-27 22:58:02 -0400

    Map Generation v1

--- commands/mapStuff.py
+++ commands/mapStuff.py
@@ -1,9 +1,33 @@
 import os
 import random as r
 
+dungeon_count = 0
+switch = {
+        1: "D",
+        2: "F",
+        3: "M",
+        4: "T"
+    }
+
+
+def area_switch(case):
+    return switch[case]
+
+
+def determine_Area():
+    global dungeon_count, switch
+
+    area = area_switch(r.randint(1, 4))
+    if area == "D" and dungeon_count != 4:
+        dungeon_count += 1
+    elif area == "D" and dungeon_count == 4:
+        area = None
+        switch[1] = "F"
+    return area
+
 
 def display_Map(world_map):  # Function to display the world map, parameter is the current explored map
-    os.system("cls")  # Clears previous text in the Terminal
+    # os.system("cls")  # Clears previous text in the Terminal
     for i in world_map:
         output = ""  # For loop to access and print individual items in the map list
         for j in i:
@@ -28,13 +52,17 @@ def gen_Map(size):
     # Creating the map as a list, _ means that no var is needed
 
     mapv2 = mapv1.copy()
-    dungeon_count = 0
+    mapv1[center[0]][center[1]] = "H"
 
     for index, i in enumerate(mapv1):
         for index2, j in enumerate(i):
-            print("[{}][{}] = {}".format(index, index2, j))
+            area = determine_Area()
+            if area is None:
+                area = determine_Area()
+                mapv2[index][index2] = area
 
+    mapv2[center[0]][center[1]] = "H"
     return [mapv1, mapv2]
 
 
-
+display_Map(gen_Map(2)[0])
