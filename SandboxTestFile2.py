8ae7958828fb4463ab8d776232f999ad64e42847
xXxSovereign <michael.w.adolph@gmail.com>
2020-10-31 00:04:56 -0400

    references file for members to look on & random sandbox

--- SandboxTestFile2.py
+++ SandboxTestFile2.py
@@ -1,10 +1,3 @@
 # same as above
-import numpy as np
-
-from numpy import array as ar
-
-x = ar([[1,2,3],[4,5,6],[7,8,9]])
-
-print(x)
-
-# If this works this is really good
+print(u"\U0001D5DB")
+print("H")
