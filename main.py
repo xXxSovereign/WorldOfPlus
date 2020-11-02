a10232adb480a3e5f00e4c63ff728a73d6d846fd
xXxSovereign <michael.w.adolph@gmail.com>
2020-11-02 16:05:49 -0500

    simple gui setup

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from commands.mapStuff import *
+from tkinter import *
 
-display_Map(gen_Map())
-## :D hello uwu
+root = Tk()
+
+root.geometry("500x500")
+
+myLabel = Label(root, text="bro this gui is slick")
+myLabel.grid(row=0, column=5)
+
+myLabel2 = Label(root, text="nah this is s l i c k")
+myLabel2.grid(row=1, column=2)
+
+myLabel3 = Label(root, text="Poggers")
+myLabel3.grid(row=5, column=3)
+
+root.mainloop()
+
